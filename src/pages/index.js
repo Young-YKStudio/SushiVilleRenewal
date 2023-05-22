e6b5623d7cd6542a9497c5627f385fe8473173ef
Young-YKStudio <young@ykstudio.dev>
2023-05-22 19:23:15 -0400

    getstaticprops?

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,28 @@ import Landing from './landing/landing'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-export default function Home() {
+export default function Home(props) {
 
   const [ menu, setMenu ] = useState([])
 
-  useEffect(() => {
-    let isMounted = true
+  console.log(props)
+  // useEffect(() => {
+  //   let isMounted = true
 
-    const requestAPI = async () => {
-      if(isMounted) {
-        try {
-          const request = await axios.get(`${process.env.APP_URL}/api/menu/getAllMenu`)
-          if(request.data.success) {
-            setMenu(request.data.menu)
-          }
-        } catch (error) {
-          console.error('error fetching data', error)
-        }
-      }
-    }
-    requestAPI()
-  }, [])
+  //   const requestAPI = async () => {
+  //     if(isMounted) {
+  //       try {
+  //         const request = await axios.get(`${process.env.APP_URL}/api/menu/getAllMenu`)
+  //         if(request.data.success) {
+  //           setMenu(request.data.menu)
+  //         }
+  //       } catch (error) {
+  //         console.error('error fetching data', error)
+  //       }
+  //     }
+  //   }
+  //   requestAPI()
+  // }, [])
 
   return (
     <>
@@ -31,4 +32,14 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export async function getStatciProps() {
+  const result = await axios.get(`${process.env.APP_URL}/api/menu/getAllMenu`)
+  const data = result.data
+  return {
+    props: {
+      data: data
+    }
+  }
 }
