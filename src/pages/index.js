5f498d44c88dcfe50681cc50dcbfcd7101e91244
Young-YKStudio <young@ykstudio.dev>
2023-05-22 21:58:22 -0400

    useEffect

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,23 +7,25 @@ export default function Home(props) {
   const [ menu, setMenu ] = useState([])
 
   console.log(props)
-  // useEffect(() => {
-  //   let isMounted = true
+  useEffect(() => {
+    let isMounted = true
 
-  //   const requestAPI = async () => {
-  //     if(isMounted) {
-  //       try {
-  //         const request = await axios.get(`${process.env.APP_URL}/api/menu/getAllMenu`)
-  //         if(request.data.success) {
-  //           setMenu(request.data.menu)
-  //         }
-  //       } catch (error) {
-  //         console.error('error fetching data', error)
-  //       }
-  //     }
-  //   }
-  //   requestAPI()
-  // }, [])
+    const requestAPI = async () => {
+      if(isMounted) {
+        try {
+          const request = await fetch(`${process.env.APP_URL}/api/menu/getAllMenu`).then((response) => response.json()).then(response => setMenu(response.menu))
+          // const request = await axios.get(`${process.env.APP_URL}/api/menu/getAllMenu`)
+          // console.log(request.json())
+          // if(request.data.success) {
+          //   // setMenu(request.data.menu)
+          // }
+        } catch (error) {
+          console.error('error fetching data', error)
+        }
+      }
+    }
+    requestAPI()
+  }, [])
 
   return (
     <>
@@ -34,17 +36,24 @@ export default function Home(props) {
   )
 }
 
-export async function getStatciProps() {
-  try {
-    // const result = await axios.get(`${process.env.APP_URL}/api/menu/getAllMenu`)
-    const result = await axios.get(`https://jsonplaceholder.typicode.com/todos/1`)
-    const data = await result.data.menu.json()
-    return {
-      props: {
-        data: data
-      }
-    }
-  } catch (error) {
-    console.log(error, 'what??')
-  }
-}
+// export async function getStatciProps() {
+//   await fetch(`${process.env.APP_URL}/api/menu/getAllMenu`).then((response) => response.json()).then(response => {
+//     return {
+//       props: {
+//         menu: response.menu
+//       }
+//     }
+//   })
+//   // try {
+//   //   // const result = await axios.get(`${process.env.APP_URL}/api/menu/getAllMenu`)
+//   //   const result = await axios.get(`https://jsonplaceholder.typicode.com/todos/1`)
+//   //   const data = await result.data.menu.json()
+//   //   return {
+//   //     props: {
+//   //       data: data
+//   //     }
+//   //   }
+//   // } catch (error) {
+//   //   console.log(error, 'what??')
+//   // }
+// }
